cec82015cac361fd165ed18a3979656e1cdfc34d
souzavaltenis <valtenis.souza.1@gmail.com>
2020-09-10 01:16:51 -0300

    Adicionado BD no TextView da tela inicial

--- app/src/main/java/com/example/ipet/activities/MainActivity.java
+++ app/src/main/java/com/example/ipet/activities/MainActivity.java
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.ipet.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getConexoes();
     }
 
     /*
@@ -32,5 +38,36 @@ public class MainActivity extends AppCompatActivity {
     public void openQueroAjudarOng(View view){
         Intent intent = new Intent(this, QueroAjudarOng.class);
         startActivity(intent);
+    }
+
+    /*
+    * Acessa a quantidade de conexões já realizadas (quando o usuário entra em contato com a ong)
+    * que está dentro do documento "counter" da coleção "conexoes", é acessado em específico o
+    * atributo quantidade, que é onde esse valor está sendo guardado.
+    * */
+    public void getConexoes(){
+
+        FirebaseFirestore.getInstance().collection("conexoes")
+                .document("counter")
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        Integer qtdConexoes = documentSnapshot.get("quantidade", Integer.class);
+                        setTextQtdConexoes(qtdConexoes);
+                    }
+                });
+    }
+
+    /*
+    * Recebe a quantidade de conexões e atualiza o texto da textview de informação sobre conexões.
+    * */
+    public void setTextQtdConexoes(Integer qtd){
+
+        TextView tvApresentacao = findViewById(R.id.tvApresentacao);
+
+        String msg = "Total de " + qtd + " conexões já realizadas";
+
+        tvApresentacao.setText(msg);
     }
 }

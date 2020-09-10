86406f71e6d41e1527586e35ed3fd385280a1636
Thiago_Moreira <thiagomdias2203@gmail.com>
2020-09-10 00:54:21 -0300

    commit SplashScreen e MainActivity

--- app/src/main/java/com/example/ipet/activities/SplashActivity.java
+++ app/src/main/java/com/example/ipet/activities/SplashActivity.java
@@ -3,6 +3,7 @@ package com.example.ipet.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,9 +16,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-        //getSupportActionBar().hide();
-        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -32,7 +31,7 @@ public class SplashActivity extends AppCompatActivity {
                 }
 
             }
-        }, 2000);
+        }, 5000);
     }
 
 }
